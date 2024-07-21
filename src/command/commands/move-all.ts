06fd2d4cf2893914a2fcf3b46fb468ac23444fef
Wittawas Nakkasem <vittee@hotmail.com>
2024-07-22 01:16:36 +0700

    feat: add `move-all` command, for moving all users from a voice channel into a new voice channel

---
+++ src/command/commands/move-all.ts
@@ -0,0 +1,106 @@
+import {
+  type APIApplicationCommandOption,
+  ApplicationCommandOptionType,
+  ChannelType,
+  channelMention,
+  GuildMember
+} from "discord.js";
+
+import { chain } from "lodash";
+import pluralize from "pluralize";
+
+import type { CommandDescriptor, InteractionHandler } from "../types";
+import { mentionUsers, orderGuildMembers } from "../../utils";
+
+const declaration: APIApplicationCommandOption = {
+  name: 'move-all',
+  description: 'Move all users from a voice channel into a new voice channel',
+  type: ApplicationCommandOptionType.Subcommand,
+  options: [
+    {
+      name: 'from',
+      description: 'The voice channel to move users from',
+      type: ApplicationCommandOptionType.Channel,
+      channel_types: [ChannelType.GuildVoice],
+      required: true
+    },
+    {
+      name: 'to',
+      description: 'The designated voice channel',
+      type: ApplicationCommandOptionType.Channel,
+      channel_types: [ChannelType.GuildVoice],
+      required: true
+    },
+    {
+      name: 'with-bot',
+      description: 'Include bot users',
+      type: ApplicationCommandOptionType.Boolean,
+      required: false
+    }
+  ]
+}
+
+const commandHandler: InteractionHandler = async (interaction) => {
+  const from = interaction.options.getChannel('from');
+  const to = interaction.options.getChannel('to');
+
+  if (!from || !to) {
+    interaction.reply('Invalid channel');
+    return;
+  }
+
+  const [fromChannel, toChannel] = [from, to].map(({ id }) => interaction.client.channels.cache.get(id));
+
+  if (!fromChannel?.isVoiceBased() || !toChannel?.isVoiceBased()) {
+    interaction.reply('Invalid channel');
+    return;
+  }
+
+  if (from.id === to.id) {
+    interaction.reply('Channels are identical');
+    return;
+  }
+
+  const withBot = interaction.options.getBoolean('with-bot') ?? false;
+
+  const members = chain(Array.from(fromChannel.members.values()))
+    .filter(m => withBot || !m.user.bot)
+    .shuffle()
+    .sortBy(orderGuildMembers({
+      issuer: interaction.user,
+      reverse: false
+    }))
+    .value();
+
+  if (members.length === 0) {
+    interaction.reply(`No users in ${channelMention(fromChannel.id)}`);
+    return;
+  }
+
+  await interaction.deferReply();
+
+  const results: Array<GuildMember> = [];
+
+  for (const member of members) {
+    const moved = await member.voice.setChannel(
+      toChannel,
+      `Demanded by ${interaction.user.username}`
+    ).catch(() => false as const);
+
+    if (moved !== false) {
+      results.push(moved);
+    }
+  }
+
+  interaction.editReply([
+    `Moved ${pluralize('user', results.length, true)}`,
+    ...mentionUsers(results)
+  ].join('\n'));
+}
+
+const descriptor: CommandDescriptor = {
+  declaration,
+  commandHandler
+}
+
+export default descriptor;
--- src/command/index.ts
+++ src/command/index.ts
@@ -5,10 +5,12 @@ import {
 
 import kickAll from "./commands/kick-all";
 import kickRole from "./commands/kick-role";
+import moveAll from "./commands/move-all";
 
 export const descriptors = {
   kickAll,
   kickRole,
+  moveAll,
 }
 
 export const createCommandDeclarations = (baseCommand: string = 'reloc'): RESTPostAPIChatInputApplicationCommandsJSONBody => ({
