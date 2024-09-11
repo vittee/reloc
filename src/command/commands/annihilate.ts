d3035ac0104267a03a1b083703cbd78b96c6d206
Wittawas Nakkasem <vittee@hotmail.com>
2024-09-11 20:26:04 +0700

    fix: update the `annihilate` command to conform with the new code base

--- src/command/commands/annihilate.ts
+++ src/command/commands/annihilate.ts
@@ -31,51 +31,53 @@ const declaration: APIApplicationCommandOption = {
   ]
 }
 
-const commandHandler: InteractionHandlers = async (interaction) => {
-  if (!interaction.guild) {
-    interaction.reply('Not in a guild');
-    return;
-  }
-
-  const withBot = interaction.options.getBoolean('with-bot') ?? false;
-
-  const allChannels = await interaction.guild.channels.fetch().then(all => Array.from(all.values()));
-
-  const members = chain(allChannels)
-    .filter((c): c is VoiceBasedChannel => c?.isVoiceBased() ?? false)
-    .flatMap(c => Array.from(c.members.values()))
-    .filter(m => withBot || !m.user.bot)
-    .shuffle()
-    .sortBy(orderGuildMembers({
-      issuer: interaction.user,
-      reverse: true
-    }))
-    .value();
-
-  const reason = interaction.options.getString('reason');
-  const logMessage = reason || `Demanded by ${interaction.user.username}`;
-
-  await interaction.deferReply();
-
-  const results: Array<GuildMember> = [];
-
-  for (const member of members) {
-    const disconnected = await member.voice.disconnect(logMessage).catch(() => false as const);
-
-    if (disconnected !== false) {
-      results.push(disconnected);
+const handlers: InteractionHandlers = {
+  command: async (interaction) => {
+    if (!interaction.guild) {
+      interaction.reply('Not in a guild');
+      return;
     }
-  }
 
-  interaction.editReply([
-    `Disconnected ${pluralize('user', results.length, true)}${reason ? ` (Reason: ${reason})`: ''}`,
-    ...mentionUsers(results)
-  ].join('\n'));
+    const withBot = interaction.options.getBoolean('with-bot') ?? false;
+
+    const allChannels = await interaction.guild.channels.fetch().then(all => Array.from(all.values()));
+
+    const members = chain(allChannels)
+      .filter((c): c is VoiceBasedChannel => c?.isVoiceBased() ?? false)
+      .flatMap(c => Array.from(c.members.values()))
+      .filter(m => withBot || !m.user.bot)
+      .shuffle()
+      .sortBy(orderGuildMembers({
+        issuer: interaction.user,
+        reverse: true
+      }))
+      .value();
+
+    const reason = interaction.options.getString('reason');
+    const logMessage = reason || `Demanded by ${interaction.user.username}`;
+
+    await interaction.deferReply();
+
+    const results: Array<GuildMember> = [];
+
+    for (const member of members) {
+      const disconnected = await member.voice.disconnect(logMessage).catch(() => false as const);
+
+      if (disconnected !== false) {
+        results.push(disconnected);
+      }
+    }
+
+    interaction.editReply([
+      `Disconnected ${pluralize('user', results.length, true)}${reason ? ` (Reason: ${reason})`: ''}`,
+      ...mentionUsers(results)
+    ].join('\n'));
+  }
 }
 
 const descriptor: CommandDescriptor = {
   declaration,
-  commandHandler
+  handlers
 }
 
 export default descriptor;
